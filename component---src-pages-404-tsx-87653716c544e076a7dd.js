4ba494eb0bbc7a3ff390dfde1d16d0ef1f710437
Joshua Barber <josh@jwjb.io>
2020-10-28 21:36:21 +0000

    Updates

---
+++ component---src-pages-404-tsx-87653716c544e076a7dd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n);t.default=function(){return l.a.createElement("div",{className:"bg-gray-light dark:bg-gray-dark lg:py-32 p-12"},l.a.createElement("div",{className:"container mx-auto"},l.a.createElement("h1",{className:"lg:text-6xl text-4xl font-bold"},"404 Page"),l.a.createElement("h3",{className:"lg:text-4xl text-2xl font-semibold"},"There's nothing here.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-87653716c544e076a7dd.js.map
